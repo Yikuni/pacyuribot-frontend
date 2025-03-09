e9fa485f3cee0171cd0c9955f02c7f40f47a19ba
Yikuni <yikuni@qq.com>
2025-03-09 21:20:56 +0800

    加入配置文件

--- src/store.ts
+++ src/store.ts
@@ -1,28 +1,52 @@
 // src/store.js
 import {reactive} from 'vue';
 import PocketBase from 'pocketbase';
+import Client from "pocketbase";
+import axios from "axios";
 
-const pb = new PocketBase('https://pocketbase.yikuni.com')
-export const store = reactive({
-    // 定义全局变量
-    pb,
-    methods:{
-      login: async(email: string, password: string)=>{
-          await pb.collection('users').authWithPassword(
-              email,
-              password,
-          )
-          return pb.authStore.isValid
-      }
-    },
-    modelInfo:{
-        id: ''
-    },
-    tmp: {
-        form: {
-            email: '',
-            password: '',
-            passwordConfirm: ''
-        }
-    }
-});
+const initializeStore = async ()=>{
+    let pb: Client;
+    let config = {}
+    await fetch("/config.json").then((resp)=>resp.json())
+        .then((res)=>{
+            config = res
+            console.log(res)
+            pb = new PocketBase(res.pocketbase)
+        })
+    return reactive({
+        pb,
+        methods:{
+            login: async(email: string, password: string)=>{
+                await pb.collection('users').authWithPassword(
+                    email,
+                    password,
+                )
+                return pb.authStore.isValid
+            },
+            activateDatasource: (datasourceID: string)=>{
+                let options = {
+                    method: 'POST',
+                    url: 'http://localhost:2514/admin/datasource/activate',
+                    params: {datasource: datasourceID},
+                    headers: {
+                        Authorization: pb.authStore.record.token,
+                    }
+                };
+
+                return axios.request(options)
+            }
+        },
+        modelInfo:{
+            id: ''
+        },
+        tmp: {
+            form: {
+                email: '',
+                password: '',
+                passwordConfirm: ''
+            }
+        },
+        config:config
+    })
+}
+export const store = await initializeStore()
